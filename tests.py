33d37a49d89010e98f26822f513e47f8062d6347
Jiří Jaskowiec <jirijaskowiec@seznam.cz>
2025-06-27 10:03:48 +0200

    add: write_file

--- tests.py
+++ tests.py
@@ -1,35 +1,15 @@
-from functions.get_files_info import get_files_info
-from functions.get_files_content import get_file_content
+from functions.write_file import write_file
 
 
 def test():
-    result = get_files_info("calculator", ".")
-    print("Result for current directory:")
+    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
     print(result)
     print("")
 
-    result = get_files_info("calculator", "pkg")
-    print("Result for 'pkg' directory:")
+    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
     print(result)
 
-    result = get_files_info("calculator", "/bin")
-    print("Result for '/bin' directory:")
-    print(result)
-
-    result = get_files_info("calculator", "../")
-    print("Result for '../' directory:")
-    print(result)
-
-    result = get_file_content("calculator", "main.py")
-    print('Result for "main.py":')
-    print(result)
-
-    result = get_file_content("calculator", "pkg/calculator.py")
-    print('Result for "pkg/calculator.py":')
-    print(result)
-
-    result = get_file_content("calculator", "/bin/cat")
-    print('Result for "/bin/cat":')
+    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
     print(result)
 
 if __name__ == "__main__":
